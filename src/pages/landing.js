40a8b87f34af7a7f0d082dcd88d53e8431cd5ae2
zirbhub <zaynebissoon20@gmail.com>
2026-04-08 21:05:06 -0400

    Changes

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -29,3 +29,24 @@ export default function Landing() {
         </>
     );
 }
+
+import { Link } from "react-router-dom";
+
+export default function Landing() {
+  return (
+    <div className="page center">
+      <h1>UWI BAEWATCH 💘</h1>
+      <p>Find your campus match at UWI St. Augustine.</p>
+      <p>Swipe, match, and connect with other students.</p>
+
+      <div className="button-row">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/signup">
+          <button>Sign Up</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
